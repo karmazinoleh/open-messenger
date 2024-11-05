49573903fe3f462d30b194bac0752fc892badcfc
Oleh Karmazin <kalemzwork@gmail.com>
2024-11-05 20:16:18 +0100

    init commit

---
+++ src/main/java/kalemz/app/open_messenger/OpenMessengerApplication.java
@@ -0,0 +1,13 @@
+package kalemz.app.open_messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenMessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kalemz/app/open_messenger/OpenMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package kalemz.app.open_messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
