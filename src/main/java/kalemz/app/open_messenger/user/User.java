506146db0a46bc296960d9bac653c7dda1fcb47c
Oleh Karmazin <kalemzwork@gmail.com>
2024-11-05 22:06:19 +0100

    Update User.java

--- src/main/java/kalemz/app/open_messenger/user/User.java
+++ src/main/java/kalemz/app/open_messenger/user/User.java
@@ -37,24 +37,4 @@ public class User {
     @Column(insertable = false)
     private LocalDateTime lastModifiedDate;*/
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNickname() {
-        return nickname;
-    }
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    
 }
